748a5d9ed06ba92cfc5697e4bd9c310b37044f40
Aaron Appel <36603239+AaronAppel@users.noreply.github.com>
2024-01-21 21:12:21 -0500

    [CTRL+Tab] Added a hotkey command to navigate to the last opened file with CTRL+Tab
    Visual Studio esq last opened file navigation. Also cleaned up some plugin template code.

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { readFile } from 'fs';
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, Workspace, WorkspaceLeaf } from 'obsidian';
 
 import * as fs from 'fs';
 
@@ -14,7 +14,7 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-// Safe guards constants and search keys
+// Safe guard constants and search keys
 const g_codeBlockMarker = "```";
 const g_pluginKeyword = "live:true";
 const g_fileNameTag = "file:";
@@ -25,85 +25,47 @@ const g_functionSearchCaseSensitivityOn = false;
 const g_clobberIfFileIsOutdated = false;
 const g_insertWarningsWhenFileIsOutdated = true;
 
+let g_previousOpenedFile: TFile | null;
+let g_currentOpenedFile: TFile | null;
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() : Promise<void> {
 		await this.loadSettings();
-		console.log("onload");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Notice me!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new SettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "navigate-to-last-leaf",
+			name: "Navigate to last opened leaf",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const currentLeaf = this.app.workspace.getLeaf();
+				if (currentLeaf && g_previousOpenedFile) {
+					currentLeaf.openFile(g_previousOpenedFile);
 				}
-			}
+			},
+			hotkeys: [
+			  {
+				modifiers: ["Mod"],
+				key: "tab", // #TODO Save and load hotkey in settings
+			  },
+			],
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 
 		this.registerEvent(
 			this.app.workspace.on("active-leaf-change", async () =>{
-				const activeFile = this.app.workspace.getActiveFile();
-				if (activeFile) {
-					
-					console.log("Active file: " + activeFile.name);
-					const content = await this.app.vault.read(activeFile);
+
+				g_previousOpenedFile = g_currentOpenedFile;
+				g_currentOpenedFile = this.app.workspace.getActiveFile();
+
+				if (g_currentOpenedFile) {
+					console.log("Active file: " + g_currentOpenedFile.name);
+					const content = await this.app.vault.read(g_currentOpenedFile);
 					if (this.CanTryReplace(content)) {
 						const result =  this.ReplaceFileTagContent(content);
 						if (result[0]) {
-							await this.app.vault.modify(activeFile, result[1]);
+							await this.app.vault.modify(g_currentOpenedFile, result[1]);
 							console.log("active-leaf-change set value");
 						}
 					}
@@ -313,23 +275,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class SettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
